d3478c7b6c64abf32262e305956ba9e399cbd834
Kristina Kordova <103128983+kristinakordova@users.noreply.github.com>
2026-01-21 12:45:03 -0800

    Initialize config.py with paths and constants

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+REF_GENOME_PATH ="----"
+DATA_DIR = "/Users/kvk22/Downloads/"
+RNA_SEQ_PATH = os.path.join(DATA_DIR, "Example RNA.csv")
+VCF_PATH = os.path.join(DATA_DIR, "TCGA_LUAD.f368273c-bd2d-4b97-97ec-a04cb130af1e.wgs.GATK4_MuTect2_Pair.somatic_annotation.vcf.gz.tbi")
+WINDOW_SIZE = 16384
