5ba108558ec1bbaf4314067f8ff9c701131ef53e
Kristina Kordova <103128983+kristinakordova@users.noreply.github.com>
2026-01-21 12:36:38 -0800

    Initialize __init__.py with module information

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+"""
+AlphaGenome Analysis Toolkit
+Benchmarking and prediction classes for genomic sequence modeling.
+"""
+
+from .AlphaGenomePlotter import AlphaGenomePlotter
+from .AlphaGenomePredictor import AlphaGenomePredictor
+
+__version__ = "0.1.0"
+__all__ = ["AlphaGenomePlotter", "AlphaGenomePredictor"]
